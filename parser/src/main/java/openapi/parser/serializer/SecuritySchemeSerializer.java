6b5646fcfe7e8e24856c5a7c13bb19b9022db763
Aowss Ibrahim <aowss@yahoo.com>
2021-09-20 08:01:12 -0400

    Upgrade to java 17

--- parser/src/main/java/openapi/parser/serializer/SecuritySchemeSerializer.java
+++ parser/src/main/java/openapi/parser/serializer/SecuritySchemeSerializer.java
@@ -8,6 +8,9 @@ import openapi.model.v310.security.oauth.*;
 import openapi.parser.ParsingException;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class SecuritySchemeSerializer extends StdSerializer<SecurityScheme> {
 
@@ -27,41 +30,48 @@ public class SecuritySchemeSerializer extends StdSerializer<SecurityScheme> {
         jsonGenerator.writeStartObject();
         jsonGenerator.writeStringField("type", securityScheme.type().name());
         if (securityScheme.description() != null) jsonGenerator.writeStringField("description", securityScheme.description());
-        //  TODO: Change that to switch when java 17 is here
-        if (securityScheme instanceof ApiKey a) {
-            jsonGenerator.writeStringField("name", a.name());
-            jsonGenerator.writeStringField("in", a.in().name());
-        } else if (securityScheme instanceof Http h) {
-            jsonGenerator.writeStringField("scheme", h.scheme().name());
-            if (h.bearerFormat() != null) jsonGenerator.writeStringField("bearerFormat", h.bearerFormat());
-        } else if (securityScheme instanceof OpenIdConnect c) {
-            jsonGenerator.writeStringField("openIdConnectUrl", c.openIdConnectUrl().toString());
-        } else if (securityScheme instanceof OAuth2 o) {
-            jsonGenerator.writeFieldName("flows");
-            jsonGenerator.writeStartObject();
-            o.flows().entrySet().forEach(flow -> {
-                try {
-                    jsonGenerator.writeFieldName(flow.getKey().name());
-                    jsonGenerator.writeStartObject();
-                    OAuthFlow oAuthFlow = flow.getValue();
-                    if (oAuthFlow instanceof Implicit i) jsonGenerator.writeStringField("authorizationUrl", i.authorizationUrl().toString());
-                    if (oAuthFlow instanceof AuthorizationCode ac) {
-                        jsonGenerator.writeStringField("authorizationUrl", ac.authorizationUrl().toString());
-                        jsonGenerator.writeStringField("tokenUrl", ac.tokenUrl().toString());
-                    }
-                    if (oAuthFlow instanceof Password p) jsonGenerator.writeStringField("tokenUrl", p.tokenUrl().toString());
-                    if (oAuthFlow instanceof ClientCredentials cc) jsonGenerator.writeStringField("tokenUrl", cc.tokenUrl().toString());
-                    if (oAuthFlow.refreshUrl() != null) jsonGenerator.writeStringField("refreshUrl", oAuthFlow.refreshUrl().toString());
-                    jsonGenerator.writeObjectField("scopes", oAuthFlow.scopes());
-                    jsonGenerator.writeEndObject();
-                } catch (Exception e) {
-                    //  TODO: handle exceptions properly
-                    throw new RuntimeException(new ParsingException("Invalid OAuth 'flow' value: " + flow, e));
-                }
-            });
-            jsonGenerator.writeEndObject();
+        switch (securityScheme) {
+            case ApiKey a -> {
+                jsonGenerator.writeStringField("name", a.name());
+                jsonGenerator.writeStringField("in", a.in().name());
+            }
+            case  Http h -> {
+                jsonGenerator.writeStringField("scheme", h.scheme().name());
+                if (h.bearerFormat() != null) jsonGenerator.writeStringField("bearerFormat", h.bearerFormat());
+            }
+            case OpenIdConnect c -> jsonGenerator.writeStringField("openIdConnectUrl", c.openIdConnectUrl().toString());
+            case OAuth2 o -> {
+                jsonGenerator.writeFieldName("flows");
+                jsonGenerator.writeStartObject();
+                o.flows().entrySet().forEach(serializeOAuthFlow.apply(jsonGenerator));
+                jsonGenerator.writeEndObject();
+            }
+            case MutualTLS tls -> {}
         }
         jsonGenerator.writeEndObject();
     }
+
+    private Function<JsonGenerator, Consumer<Map.Entry<OAuthFlowType, OAuthFlow>>> serializeOAuthFlow = jsonGenerator -> flow -> {
+        try {
+            jsonGenerator.writeFieldName(flow.getKey().name());
+            jsonGenerator.writeStartObject();
+            OAuthFlow oAuthFlow = flow.getValue();
+            switch (oAuthFlow) {
+                case Implicit i -> jsonGenerator.writeStringField("authorizationUrl", i.authorizationUrl().toString());
+                case AuthorizationCode ac -> {
+                    jsonGenerator.writeStringField("authorizationUrl", ac.authorizationUrl().toString());
+                    jsonGenerator.writeStringField("tokenUrl", ac.tokenUrl().toString());
+                }
+                case Password p -> jsonGenerator.writeStringField("tokenUrl", p.tokenUrl().toString());
+                case ClientCredentials cc -> jsonGenerator.writeStringField("tokenUrl", cc.tokenUrl().toString());
+            }
+            if (oAuthFlow.refreshUrl() != null) jsonGenerator.writeStringField("refreshUrl", oAuthFlow.refreshUrl().toString());
+            jsonGenerator.writeObjectField("scopes", oAuthFlow.scopes());
+            jsonGenerator.writeEndObject();
+        } catch (Exception e) {
+            //  TODO: handle exceptions properly
+            throw new RuntimeException(new ParsingException("Invalid OAuth 'flow' value: " + flow, e));
+        }
+    };
 
 }
